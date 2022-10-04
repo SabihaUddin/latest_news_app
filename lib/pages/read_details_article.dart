189d90665518f54fcba28665681fe65f74b816e3
sabiha <sabiha.uddin786@gmail.com>
2022-10-05 00:55:29 +0600

    design article page

--- lib/pages/read_details_article.dart
+++ lib/pages/read_details_article.dart
@@ -16,89 +16,149 @@ class _DetailArticlePageState extends State<DetailArticlePage> {
 
   @override
   Widget build(BuildContext context) {
-    size = MediaQuery
-        .of(context)
-        .size;
+    size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.transparent,
-        body: Stack(children: [
-          Positioned.fill(
-              child: Image.network(
-                'https://i.pinimg.com/564x/dd/b1/24/ddb1244ae23de5fa7c85f00906cfbada.jpg',
-                fit: BoxFit.cover,
-              )),
-          ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(children: [
+        Positioned.fill(
+            child: Image.network(
+          'https://i.pinimg.com/564x/dd/b1/24/ddb1244ae23de5fa7c85f00906cfbada.jpg',
+          fit: BoxFit.cover,
+        )),
+        ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomTag(backgroundColor: Colors.black, children: [
+                    Text(
+                      'Transport',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(color: Colors.orange),
+                    ),
+                  ]),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Lorem Ipsum is simply dummy text of the printing .',
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        height: 1.25,
+                        color: Colors.orange),
+                  ),
+                  Text(
+                    'Lorem Ipsum is simply dummy text of the printing .',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(color: Colors.orange),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       CustomTag(backgroundColor: Colors.black, children: [
+                        CircleAvatar(
+                          child: Image.network(
+                              'https://i.pinimg.com/564x/dd/b1/24/ddb1244ae23de5fa7c85f00906cfbada.jpg'),
+                          radius: 10,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
                         Text(
-                          'Transport',
+                          "Manika",
                           style: Theme.of(context)
                               .textTheme
                               .bodyMedium!
-                              .copyWith(color: Colors.orange),
-                        ),
+                              .copyWith(color: Colors.white),
+                        )
                       ]),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Lorem Ipsum is simply dummy text of the printing .',
-                        style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            height: 1.25,
-                            color: Colors.orange),
-                      ),
-                      Text(
-                        'Lorem Ipsum is simply dummy text of the printing .',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyLarge!
-                            .copyWith(color: Colors.orange),
-                      ),
+                      CustomTag(
+                          backgroundColor: const Color(0xFFEEEEEE),
+                          children: [
+                            const Icon(
+                              Icons.timelapse_outlined,
+                              color: Colors.black,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text('2h ago',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(
+                                      color: Colors.black,
+                                    )),
+                          ]),
+                      CustomTag(
+                          backgroundColor: Colors.grey.shade200,
+                          children: [
+                            const Icon(
+                              Icons.visibility,
+                              color: Colors.black,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text('376',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(
+                                      color: Colors.black,
+                                    )),
+                          ])
                     ],
                   ),
-                ),
-                Container(
-
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20)),
-                    color: Colors.white,
+                  const SizedBox(
+                    height: 15,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        CustomTag(backgroundColor: Colors.black, children: [
-                          CircleAvatar(
-                            child: Image.network('https://i.pinimg.com/564x/dd/b1/24/ddb1244ae23de5fa7c85f00906cfbada.jpg'),
-                            radius: 10,
-                          ),
-                          Text("Manika")
-                        ])
-                      ],
-                    ),
+                  Text(
+                    'Lorem Ipsum is simply dummy text ',
+                    style: Theme.of(context).textTheme.headline6!.copyWith(
+                          color: Colors.black,
+                        ),
                   ),
-                )
-              ],
-          ),
-        ]
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrystandard dummy text ever since the 1500s, when',
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Colors.black,))
+                ],
+              ),
+            )
+          ],
         ),
+      ]),
       appBar: AppBar(
         iconTheme: const IconThemeData(
           color: Colors.white,
         ),
-        leading: Icon(Icons.arrow_back),
+        leading: const Icon(Icons.arrow_back),
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-
     );
   }
 }
